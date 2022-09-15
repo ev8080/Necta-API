91ab3a9c348ff9554f9c8461678880cf87a282c9
Bwire Mashauri <35838987+255programmer@users.noreply.github.com>
2022-09-15 16:20:31 +0300

    Delete config.php

--- napi/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('NECTA_URL', 'https://onlinesys.necta.go.tz/results/');
-define('LEVELS', 'CSEE,ACSEE');
-define('O_LIMITS', '2015,2016,2017,2018,2019,2020');
-define('A_LIMITS', '2014,2015,2016,2017,2018,2019,2020,2021');
-?>
